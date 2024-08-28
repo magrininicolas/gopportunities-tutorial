ebfc40f2976f25d066cd69b83babf2de3669f3e7
magrininicolas <magrininicolas@outlook.com>
2024-08-28 11:55:52 -0300

    Decoupling handlers

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/magrininicolas/gopportunities-tutorial/router"
+import (
+	"github.com/magrininicolas/gopportunities-tutorial/config"
+	"github.com/magrininicolas/gopportunities-tutorial/router"
+)
 
 func main() {
+	logger := *config.GetLogger("main")
+	if err := config.Init(); err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
